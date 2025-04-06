dafe64812b1110c23b64d1f4f2587a414a4af33e
MOHAMMED_ZULQARNAIN <zulqarnain4292@gmail.com>
2025-04-06 13:13:27 +0530

    vite.config.js modify

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['pdftoxml-app.onrender.com']
+  }
 })
